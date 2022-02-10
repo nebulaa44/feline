497c2f18af1f4e088baace3e21da63789899ce8c
kai <gmdezreal@gmail.com>
2022-02-10 08:55:28 -0600

    make debug info more readable

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use config::Config;
 fn main() 
 {
     let config = Config::new();
-    if config.switches.contains(&String::from("--dbg")) { println!("Config data: {config:#?}"); }
+    if config.switches.contains(&String::from("--dbg")) { println!("Config data\n{config:#?}\n"); }
 
     if config.filenames.len() == 0 { config.copy_stdin(); }
 }
