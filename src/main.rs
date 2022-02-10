32716bbfd13ea82ea6702773bfbe3b0e006cd243
kai <gmdezreal@gmail.com>
2022-02-09 21:08:02 -0600

    actually implement copy_stdin()

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::io;
 fn main() 
 {
     let args: Vec<String> = env::args().collect();
-    println!("{}", args.len());
 
     // There will always be at least 1 argument: the binary path.
     if args.len() == 1 { copy_stdin(); }
@@ -12,5 +11,17 @@ fn main()
 
 fn copy_stdin()
 {
-    unimplemented!();
+    let mut input_buf = String::new();
+
+    loop 
+    {
+        io::stdin()
+            .read_line(&mut input_buf)
+            .expect("Could not read standard input!");
+
+        // We print! because read_line adds a newline for us.
+        print!("{}", input_buf);
+
+        input_buf.clear();
+    }
 }
